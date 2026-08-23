679147dc6b1a72b7f225f7366008a5b7969c2465
kevadiaaakash2906 <kevadiaaakash@gmail.com>
2026-08-23 21:41:15 +0530

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -6,3 +6,5 @@ $('filterCustomer').addEventListener('change', function() { window.currentPage =
 $('filterDateFrom').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
 $('filterDateTo').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
 $('filterSaleStatus').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
+$('filterSoldTo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
+$('filterMemoNo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
