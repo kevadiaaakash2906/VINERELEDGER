bad55802b69c5bf3adf8d40849964aded5844bdf
kevadiaaakash2906 <kevadiaaakash@gmail.com>
2026-08-24 14:17:03 +0530

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -2,9 +2,5 @@
    VINÉRE — Filters
    ============================================ */
 
-$('filterCustomer').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
-$('filterDateFrom').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
-$('filterDateTo').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
-$('filterSaleStatus').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
 $('filterSoldTo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
 $('filterMemoNo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
