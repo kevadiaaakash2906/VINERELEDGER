287a7aef3ab4e567408096450300d263346f09ae
kevadiaaakash2906 <kevadiaaakash@gmail.com>
2026-08-25 09:17:18 +0530

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -4,3 +4,4 @@
 
 $('filterSoldTo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
 $('filterMemoNo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
+$('filterPaymentStatus').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
