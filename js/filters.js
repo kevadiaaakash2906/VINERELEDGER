3d708bf939797f9138cc25678ae9c8b72d4bb790
kevadiaaakash2906 <kevadiaaakash@gmail.com>
2026-08-25 10:49:14 +0530

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -5,3 +5,5 @@
 $('filterSoldTo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
 $('filterMemoNo').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
 $('filterPaymentStatus').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
+$('filterExpenseCategory').addEventListener('change', function() { window.currentPage = 1; renderAll(); });
+$('filterExpenseSeller').addEventListener('input', function() { window.currentPage = 1; renderAll(); });
